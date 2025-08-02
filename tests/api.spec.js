285b0be80f1ef17c7ba57ad69410409a7ff907e6
ChavhanRahul <chavhanrahulofficial@gmail.com>
2025-08-02 10:59:03 +0530

    POM update

---
+++ tests/api.spec.js
@@ -0,0 +1,8 @@
+const {test, expect} = require('@playwright/test')
+test('Validate API Response', async ({page}) =>{
+    const response = await request.post('https://www.w3school.com',{data: {usename: 'Rahul', password: 'secret'}})
+    expect(response.status()).toBe(201)
+    const body = await response.json()
+    expect(body.token).toBeDefined()
+
+})
--- tests/suite.spec.js
+++ tests/suite.spec.js
@@ -1,3 +1,6 @@
 import './google.spec.js';
 import './irctc.spec.js';
+import './example.spec.js'
+import './flipkart.spec.js'
+
 // npx playwright test ./tests/interview.spec.js ./tests/google.spec.js
