ed7ee50a32e2fd34b01899cf51d97d7ed6ac0c86
ChavhanRahul <chavhanrahulofficial@gmail.com>
2025-08-23 00:07:06 +0530

    config issue

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,14 +1,4 @@
-// @ts-check
 import { defineConfig, devices } from '@playwright/test';
-
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
-
 /**
  * @see https://playwright.dev/docs/test-configuration
  */
@@ -29,39 +19,34 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: 'http://localhost:3000',
-
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-    timeout : 120_000,
-    expect: {timeout: 120_000},
-
+   
   },
-  // require('dotenv').config(),
-
+  /* Set the maximum time one test can run for */
+    timeout: 30_000,
+    expect: {timeout: 30_000},
   /* Configure projects for major browsers */
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'] ,
+      use: { 
+      ...devices['Desktop Chrome'] ,
       viewport: { width: 1280, height: 720 },
       screenshot: 'on',
       video: 'off',
       trace: 'retain-on-failure',
       storageState: 'storageState.json'
-
     }
   }
-
     // {
     //   name: 'firefox',
     //   use: { ...devices['Desktop Firefox'] },
     // },
-
     // {
     //   name: 'webkit',
     //   use: { ...devices['Desktop Safari'] },
     // },
-
     /* Test against mobile viewports. */
     // {
     //   name: 'Mobile Chrome',
@@ -71,7 +56,6 @@ export default defineConfig({
     //   name: 'Mobile Safari',
     //   use: { ...devices['iPhone 12'] },
     // },
-
     /* Test against branded browsers. */
     // {
     //   name: 'Microsoft Edge',
@@ -82,7 +66,6 @@ export default defineConfig({
     //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
     // },
   ],
-
   /* Run your local dev server before starting the tests */
   // webServer: {
   //   command: 'npm run start',
@@ -90,4 +73,3 @@ export default defineConfig({
   //   reuseExistingServer: !process.env.CI,
   // },
 });
-
